d4885f887aab787998eba1562555714bf3d7e177
optimlab <admin@optimlab.com>
2018-07-03 23:31:19 +0300

    information.js

--- optimblog-category.ocmod/upload/catalog/view/javascript/information.js
+++ optimblog-category.ocmod/upload/catalog/view/javascript/information.js
@@ -1,9 +1,10 @@
 /**
- * @author Dmitriy Khokhlov <admin@optimlab.com>
- * @copyright 2018
- * @name optimblog
- * @version 3.0.0.1
- * @link optimlab.com
+ * @package    OptimBlog
+ * @version    3.0.0.1
+ * @author     Dmitriy Khokhlov <admin@optimlab.com>
+ * @copyright  Copyright (c) 2018, Dmitriy Khokhlov. (http://optimlab.com/)
+ * @license    https://opensource.org/licenses/GPL-3.0
+ * @link       http://optimlab.com
  */
 $(document).ready(function() {
 	// Information List
