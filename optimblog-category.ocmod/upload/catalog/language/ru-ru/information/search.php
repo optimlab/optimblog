40a5130f21fae736bd5b4bef329f6593610dd9f9
optimlab <admin@optimlab.com>
2018-12-24 13:13:03 +0300

    Seo Url moved and BugFix
    BugFix Exclusion Articles.
    Seo Url moved to optimblog-category.ocmod.

--- optimblog-category.ocmod/upload/catalog/language/ru-ru/information/search.php
+++ optimblog-category.ocmod/upload/catalog/language/ru-ru/information/search.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package    OptimBlog
- * @version    3.0.0.3
+ * @version    3.0.0.5
  * @author     Dmitriy Khokhlov <admin@optimlab.com>
  * @copyright  Copyright (c) 2018, Dmitriy Khokhlov. (http://optimlab.com/)
  * @license    https://opensource.org/licenses/GPL-3.0
--- optimblog-category.ocmod/upload/catalog/view/javascript/information.js
+++ optimblog-category.ocmod/upload/catalog/view/javascript/information.js
@@ -1,6 +1,6 @@
 /**
  * @package    OptimBlog
- * @version    3.0.0.3
+ * @version    3.0.0.5
  * @author     Dmitriy Khokhlov <admin@optimlab.com>
  * @copyright  Copyright (c) 2018, Dmitriy Khokhlov. (http://optimlab.com/)
  * @license    https://opensource.org/licenses/GPL-3.0
