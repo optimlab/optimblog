0598d908f8d5ef90382c043666bf4009f388e4f2
optimlab <admin@optimlab.com>
2020-06-02 03:38:32 +0300

    Update Italiano
    Traduzione richiesta

---
+++ src/languages/it-it/optimblog.it-it.ocmod/upload/admin/language/it-it/extension/information/optimblog.php
@@ -0,0 +1,134 @@
+<?php
+/**
+ * @package    OptimBlog
+ * @version    3.0.1.0
+ * @author     Dmitriy Khokhlov <admin@optimlab.com>
+ * @copyright  Copyright (c) 2018, Dmitriy Khokhlov. (http://optimlab.com/)
+ * @license    https://opensource.org/licenses/GPL-3.0
+ * @link       http://optimlab.com
+ */
+// Heading
+$_['heading_title']                            = 'OptimBlog';
+
+// Text
+$_['text_extension']                           = 'Estensioni';
+$_['text_success']                             = 'Hai modificato OptimBlog con successo!';
+$_['text_edit']                                = 'Modifica OptimBlog';
+$_['text_general']                             = 'Generale';
+$_['text_exclusion']                           = 'Esclusione';
+$_['text_information']                         = 'Informazioni';
+$_['text_author']                              = 'Autore';
+$_['text_date']                                = 'Data';
+$_['text_manufacturer']                        = 'Produttore';
+$_['text_review']                              = 'Recensioni';
+$_['text_view']                                = 'Visualizza';
+$_['text_sort']                                = 'Ordinamento';
+$_['text_limit']                               = 'Limite';
+$_['text_image']                               = 'Immagini';
+$_['text_default']                             = 'Predefinito';
+$_['text_date_asc']                            = 'Data (Pi&ugrave; vecchi)';
+$_['text_date_desc']                           = 'Data (Ultimi)';
+$_['text_name_asc']                            = 'Nome (A - Z)';
+$_['text_name_desc']                           = 'Nome (Z - A)';
+$_['text_rating_asc']                          = 'Recensione (Alta)';
+$_['text_rating_desc']                         = 'Recensione (Bassa)';
+//$_['text_share']                               = 'Share';
+//$_['text_seo']                                 = 'SEO';
+//$_['text_backup']                              = 'Backup &amp; Restore';
+//$_['text_import']                              = 'Your file was successfully imported!';
+
+// Entry
+$_['entry_status']                             = 'Stato';
+$_['entry_information']                        = 'Articoli';
+$_['entry_information_limit']                  = 'Elementi per pagina';
+$_['entry_information_description_length']     = 'Limite descrizione';
+$_['entry_information_count']                  = 'Conteggio articoli categoria';
+$_['entry_information_author']                 = 'Mostra autore';
+$_['entry_information_date']                   = 'Mostra data';
+$_['entry_information_review']                 = 'Mostra Recensioni';
+$_['entry_information_manufacturer']           = 'Mostra produttore';
+$_['entry_information_show']                   = 'Mostra articoli nelle categorie';
+$_['entry_category_author']                    = 'Mostra autore';
+$_['entry_category_date']                      = 'Mostra data';
+$_['entry_category_review']                    = 'Mostra Recensioni';
+$_['entry_category_view']                      = 'Visualizzazione predefinita';
+$_['entry_category_sort']                      = 'Ordinamento';
+$_['entry_category_sort_show']                 = 'Mostra ordinamento';
+$_['entry_category_limit_show']                = 'Mostra limite';
+$_['entry_category_view_show']                 = 'Mostra i pulsanti';
+$_['entry_image_category']                     = 'Dimensione Immagine Categoria (L x A)';
+$_['entry_image_thumb']                        = 'Immagine dell\'articolo - Dimensione dell\'Anteprima (L x A)';
+$_['entry_image_popup']                        = 'Immagine dell\'articolo - Dimensione Popup (L x A)';
+$_['entry_image_information']                  = 'Immagine dell\'articolo - Dimensione Elenco (L x A)';
+$_['entry_image_additional']                   = 'Dimensione dell\'Immagine dell\'articolo aggiuntiva (L x A)';
+$_['entry_image_related']                      = 'Dimensione dell\'Immagine dell\'articolo correlato (L x A)';
+$_['entry_image_category_popup']               = 'Immagine della Categoria - Dimensione Popup (L x A)';
+$_['entry_image_category_additional']          = 'Immagine Aggiuntiva della Categoria (L x A)';
+$_['entry_width']                              = 'Larghezza';
+$_['entry_height']                             = 'Altezza';
+$_['entry_review']                             = 'Permetti recensioni';
+$_['entry_review_guest']                       = 'Permetti recensioni degli ospiti';
+$_['entry_captcha']                            = 'Captcha';
+$_['entry_category']                           = 'Categorie';
+$_['entry_exclusion_information']              = 'Articoli';
+$_['entry_exclusion_information_author']       = 'Mostra autore';
+$_['entry_exclusion_information_date']         = 'Mostra data';
+$_['entry_exclusion_information_review']       = 'Mostra Recensioni';
+$_['entry_exclusion_information_manufacturer'] = 'Mostra produttore';
+$_['entry_exclusion_category_author']          = 'Mostra autore';
+$_['entry_exclusion_category_date']            = 'Mostra data';
+$_['entry_exclusion_category_review']          = 'Mostra Recensioni';
+//$_['entry_share']                              = 'Entry Code';
+//$_['entry_canonical_category_product']         = 'Canonical URL Category Products';
+//$_['entry_canonical_category_information']     = 'Canonical URL Category Articles';
+//$_['entry_canonical_information']              = 'Canonical URL Article';
+//$_['entry_breadcrumbs_category_product']       = 'Breadcrumbs Category Products';
+//$_['entry_breadcrumbs_category_information']   = 'Breadcrumbs Category Articles';
+//$_['entry_breadcrumbs_product']                = 'Breadcrumbs Product';
+//$_['entry_breadcrumbs_information']            = 'Breadcrumbs Article';
+//$_['entry_backup']                             = 'Backup';
+//$_['entry_restore']                            = 'Restore';
+
+// Tab
+$_['tab_category']                             = 'Categoria';
+$_['tab_exclusion']                            = 'Esclusione';
+
+// Help
+$_['help_category_view']                       = 'Definisce il layout degli articoli visualizzandolo nella pagina della categoria.';
+$_['help_category_sort']                       = 'Determina l\'ordine in cui gli articoli compaiono nella pagina della categoria.';
+$_['help_information_limit']                   = 'Determina quanti articoli compaiono nella pagina della categoria.';
+$_['help_information_description_length']      = 'Il numero di caratteri nella breve descrizione dell\'articolo nella categoria.';
+$_['help_review']                              = 'Abilita / Disabilita nuove recensioni e visualizza le recensioni esistenti.';
+$_['help_review_guest']                        = 'Consenti agli ospiti di pubblicare recensioni.';
+$_['help_exclusion_information']               = '(Completamento automatico)';
+$_['help_exclusion_category']                  = '(Completamento automatico)';
+$_['help_information_count']                   = 'Mostra il numero di articoli all\'interno delle sottocategorie nel menu delle Categorie. Attenzione, questo provocher&agrave; una richiesta di prestazioni estreme nei negozi con molte sottocategorie!';
+$_['help_captcha']                             = 'Captcha da utilizzare per le recensioni.';
+//$_['help_canonical_category']                  = 'Adds the parent categories. Example: /category/subcategory/subcategory';
+//$_['help_canonical_information']               = 'Adds the parent categories, if the Main Category is available in its settings. Example: /category/subcategory/article';
+//$_['help_breadcrumbs_category']                = 'Full path with a short URL /subcategory. Example: /category/subcategory/subcategory';
+//$_['help_breadcrumbs_product']                 = 'Full path with a short URL /product, if the Main Category is available in its settings. Example: /category/subcategory/product.';
+//$_['help_breadcrumbs_information']             = 'Full path with a short URL /article, if the Main Category is available in its settings. Example: /category/subcategory/article.';
+//$_['help_backup']                              = 'Backup Settings from your DateBase';
+//$_['help_restore']                             = 'Restore Settings into your DateBase';
+
+// Buttons
+$_['button_block']                             = 'In blocco';
+$_['button_list']                              = 'Elenco';
+$_['button_grid']                              = 'Griglia';
+
+// Error
+$_['error_warning']                            = 'Attenzione: controlla attentamente il modulo per trovare gli errori!';
+$_['error_permission']                         = 'Attenzione: non hai i permessi per modificare OptimBlog!';
+//$_['error_file']                               = 'File could not be found!';
+//$_['error_filetype']                           = 'Invalid file type!';
+//$_['error_datatype']                           = 'Invalid data type!';
+$_['error_limit']                              = 'Limite articolo richiesto!';
+$_['error_image_thumb']                        = 'Immagine dell\'articolo - Dimensione dell\'Anteprima richiesta!';
+$_['error_image_popup']                        = 'Immagine dell\'articolo - Dimensione Popup richiesta!';
+$_['error_image_information']                  = 'Dimensione elenco Articoli richiesta!';
+$_['error_image_category']                     = 'Dimensione elenco Categorie richiesta!';
+$_['error_image_additional']                   = 'Dimensione dell\'Immagine dell\'articolo aggiuntiva richiesta!';
+$_['error_image_related']                      = 'Dimensione dell\'Immagine dell\'articolo correlato richiesta!';
+$_['error_image_category_popup']               = 'Immagine della Categoria - Dimensione Popup richiesta!';
+$_['error_image_category_additional']          = 'Dimensione dell\'immagine Aggiuntiva della Categoria richiesta!';
