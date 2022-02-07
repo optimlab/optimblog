db75e6bf1d01ee0e9813f2636275846bed9fab50
optimlab <admin@optimlab.com>
2022-02-07 15:38:11 +0300

    3.1.0.1

--- src/optimblog.ocmod/upload/admin/controller/extension/module/optimblog.php
+++ src/optimblog.ocmod/upload/admin/controller/extension/module/optimblog.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package    OptimBlog
- * @version    3.1.0.0
+ * @version    3.1.0.1
  * @author     Dmitriy Khokhlov <admin@optimlab.com>
  * @copyright  Copyright (c) 2018, Dmitriy Khokhlov. (https://optimlab.com/)
  * @license    https://opensource.org/licenses/GPL-3.0
@@ -9,7 +9,7 @@
  */
 class ControllerExtensionModuleOptimBlog extends Controller {
 	private $error = array();
-	private $version = '3.1.0.0';
+	private $version = '3.1.0.1';
 	private $github = 'https://api.github.com/repos/optimlab/optimblog';
 	private $releases = '/releases';
 	private $latest = '/latest';
--- src/optimblog.ocmod/upload/catalog/model/extension/module/optimblog_review.php
+++ src/optimblog.ocmod/upload/catalog/model/extension/module/optimblog_review.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package    OptimBlog
- * @version    3.1.0.0
+ * @version    3.1.0.1
  * @author     Dmitriy Khokhlov <admin@optimlab.com>
  * @copyright  Copyright (c) 2018, Dmitriy Khokhlov. (https://optimlab.com/)
  * @license    https://opensource.org/licenses/GPL-3.0
